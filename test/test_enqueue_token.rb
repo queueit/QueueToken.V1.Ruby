485011aef9028b62fff1fa521ca0c32e49481e2b
Kevin Robatel <kevin.robatel@qoqa.com>
2019-09-04 11:27:28 +0200

    Refactoring tests

--- test/test_enqueue_token.rb
+++ test/test_enqueue_token.rb
@@ -2,18 +2,24 @@ require 'minitest/autorun'
 require 'queue_it_token'
 
 class EnqueueTokenTest < Minitest::Test
+  def setup
+    @customer_id = 'ticketania'
+    @token_identifier = 'a21d423a-43fd-4821-84fa-4390f6a2fd3e'
+    @secret = '5ebbf794-1665-4d48-80d6-21ac34be7faedf9e10b3-551a-4682-bb77-fee59d6355d6'
+  end
+
   def test_minimum_header
     expected_signed_token = 'eyJ0eXAiOiJRVDEiLCJlbmMiOiJBRVMyNTYiLCJpc3MiOjE1MzQ3MjMyMDAwMDAsInRpIjoiYTIxZDQyM2EtNDNm' \
                             'ZC00ODIxLTg0ZmEtNDM5MGY2YTJmZDNlIiwiYyI6InRpY2tldGFuaWEifQ..ChCRF4bTbt4zlOcvXLjQYouhgqgi' \
                             'NNNZqcci8VWoZIU'
 
-    enqueue_token = EnqueueToken.new(
-      customer_id: 'ticketania',
-      token_identifier: 'a21d423a-43fd-4821-84fa-4390f6a2fd3e',
+    enqueue_token = QueueItToken::EnqueueToken.new(
+      customer_id: @customer_id,
+      token_identifier: @token_identifier,
       issued_at: 1_534_723_200_000
     )
 
-    token = enqueue_token.token('5ebbf794-1665-4d48-80d6-21ac34be7faedf9e10b3-551a-4682-bb77-fee59d6355d6')
+    token = enqueue_token.token(@secret)
     assert_equal expected_signed_token, token
   end
 
@@ -23,9 +29,9 @@ class EnqueueTokenTest < Minitest::Test
                             'OiJteWV2ZW50IiwiaXAiOiI1LjcuOC42IiwieGZmIjoiNDUuNjcuMi40LDM0LjU2LjMuMiJ9..wUOdVDIKlrIqum' \
                             'pU33bShDPdvTkicRk3q4Z-Vs8epFc'
 
-    enqueue_token = EnqueueToken.new(
-      customer_id: 'ticketania',
-      token_identifier: 'a21d423a-43fd-4821-84fa-4390f6a2fd3e',
+    enqueue_token = QueueItToken::EnqueueToken.new(
+      customer_id: @customer_id,
+      token_identifier: @token_identifier,
       event_id: 'myevent',
       issued_at: 1_534_723_200_000,
       expire_at: 1_539_129_600_000,
@@ -33,7 +39,7 @@ class EnqueueTokenTest < Minitest::Test
       ip_forwared_for: '45.67.2.4,34.56.3.2'
     )
 
-    token = enqueue_token.token('5ebbf794-1665-4d48-80d6-21ac34be7faedf9e10b3-551a-4682-bb77-fee59d6355d6')
+    token = enqueue_token.token(@secret)
     assert_equal expected_signed_token, token
   end
 
@@ -43,7 +49,7 @@ class EnqueueTokenTest < Minitest::Test
                             'OiJteWV2ZW50In0.0rDlI69F1Dx4Twps5qD4cQrbXbCRiezBd6fH1PVm6CnVY456FALkAhN3rgVrh_PGCJHcEXN5' \
                             'zoqFg65MH8WZc_CQdD63hJre3Sedu0-9zIs.aZgzkJm57etFaXjjME_-9LjOgPNTTqkp1aJ057HuEiU'
 
-    payload = EnqueueTokenPayload.new(
+    payload = QueueItToken::EnqueueTokenPayload.new(
       key: 'somekey',
       relative_quality: 0.45678663514,
       custom_data: {
@@ -51,16 +57,16 @@ class EnqueueTokenTest < Minitest::Test
         size: 'medium'
       }
     )
-    enqueue_token = EnqueueToken.new(
-      customer_id: 'ticketania',
-      token_identifier: 'a21d423a-43fd-4821-84fa-4390f6a2fd3e',
+    enqueue_token = QueueItToken::EnqueueToken.new(
+      customer_id: @customer_id,
+      token_identifier: @token_identifier,
       event_id: 'myevent',
       issued_at: 1_534_723_200_000,
       expire_at: 1_539_129_600_000,
       payload: payload
     )
 
-    token = enqueue_token.token('5ebbf794-1665-4d48-80d6-21ac34be7faedf9e10b3-551a-4682-bb77-fee59d6355d6')
+    token = enqueue_token.token(@secret)
     assert_equal expected_signed_token, token
   end
 end
